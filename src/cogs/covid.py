00213bbf216e93907c1d8a67e92746f6391a331e
Cr4zi <ori.mizrahi1000@gmail.com>
2020-12-29 19:34:27 +0200

    sending embed with all data

--- src/cogs/covid.py
+++ src/cogs/covid.py
@@ -9,7 +9,19 @@ class Covid(commands.Cog):
 
     @commands.command()
     async def stats(self, ctx, country):
-        await ctx.send('work')
+        response = requests.get(f'https://api.covid19api.com/dayone/country/{country}')
+        data = response.json()
+
+        embed = discord.Embed(
+            title=f"{data[-1]['Country']} covid19 statistics",
+            color=0xe74c3c if int(data[-1]['Deaths']) > int(data[-1]['Recovered']) else 0x2ecc71
+        )
+        embed.add_field(name='Confirmed', value=f"{data[-1]['Confirmed']}", inline=False)
+        embed.add_field(name='Deaths', value=f"{data[-1]['Deaths']}", inline=False)
+        embed.add_field(name='Recovered', value=f"{data[-1]['Recovered']}", inline=False)
+        embed.add_field(name='Active', value=f"{data[-1]['Active']}", inline=False)
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
