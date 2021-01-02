35806423e67dcbd0dca07e3131415021363a28f3
Cr4zi <ori.mizrahi1000@gmail.com>
2021-01-02 14:46:18 +0200

    Making a change status`

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
+from discord.ext.tasks import loop
 import requests
+from itertools import cycle
 
 import os
 from dotenv import load_dotenv
@@ -18,10 +20,21 @@ intents = discord.Intents(
 
 client = commands.Bot(command_prefix='*', intents=intents)
 client.remove_command('help')
+covid_summary = requests.get('https://api.covid19api.com/summary', verify=False)
+global_info = covid_summary.json()['Global']
+status_list = cycle([f'{global_info["TotalConfirmed"]} total confirmed', \
+                     f'{global_info["TotalDeaths"]} total deaths', \
+                     f'{global_info["TotalRecovered"]} total recovered'])
+
+
+@loop(seconds=10)
+async def change_status():
+    await client.change_presence(activity=discord.Game(name=next(status_list)))
 
 
 @client.event
 async def on_ready():
+    change_status.start()
     print(f'Connected as {client.user}')
 
 initial_cogs = ['cogs.covid', 'cogs.helpcommand']
